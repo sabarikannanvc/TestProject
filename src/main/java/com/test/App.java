b783f692a6e8cfe523ee2d6f195716a5a87c15e1
jothi <jothi_kannan@hcl.com>
2024-06-18 09:14:01 +0000

    first line modified in master

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -8,7 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("1st line dev1");
+        
+	System.out.println("1st line dev1 is changed from master local");
         System.out.println( "Hello World!" );
 	System.out.println( "Local dev1 added this line");
         System.out.println("line1 Added in main");
