41b928dfd503421de6d134cd9ece6dd96b70d002
jothi <jothi_kannan@hcl.com>
2024-06-18 09:21:56 +0000

    line 2 added

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -11,6 +11,7 @@ public class App
         
 	System.out.println("1st line dev1 is changed from master local");
         System.out.println( "Hello World!" );
+	System.out.println( "line2 added in dev1");
 	System.out.println( "Local dev1 added this line");
         System.out.println("line1 Added in main");
     }
